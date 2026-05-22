52a1c5a965fa8cdc072c9f7cff684916001e31bc
dibyansu2611 <dibyansu.mahapatra@rumango.com>
2026-05-22 14:45:40 +0530

    Added: Docker Config for UI in Container

--- src/main/java/com/shorturl/config/CorsConfig.java
+++ src/main/java/com/shorturl/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "https://c-url.lovable.app.lovable.app")
+						.allowedOrigins("http://localhost:8081", "https://c-url.lovable.app.lovable.app","https://happy-indexed-rate-few.trycloudflare.com")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true);
 			}
