f1edce45abea0b8210f8a6895ed1f6dcbc489248
dibyansu2611 <dibyansu.mahapatra@rumango.com>
2026-05-27 18:05:08 +0530

    Modified: Config to avoid CORS issue

--- src/main/java/com/shorturl/config/CorsConfig.java
+++ src/main/java/com/shorturl/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:80", "https://c-url.lovable.app.lovable.app","https://happy-indexed-rate-few.trycloudflare.com")
+						.allowedOrigins("http://localhost", "http://localhost:5173", "http://localhost:8081")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true);
 			}
